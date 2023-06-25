d4f2598e9a76f0d50ab179649c7983ec292f5d6f
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-06-25 14:36:01 -0300

    MainLayout component

---
+++ src/layouts/main/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useState } from "react";
+import Menu from "../../components/menu";
+import userPhoto from "../../assets/common/user-photo.png";
+import { ReactComponent as Logo } from "../../assets/common/logo.svg";
+import "./styles.css";
+
+function MainLayout({ children }: { children: ReactNode }) {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(document.body.clientWidth > 992);
+
+    window.addEventListener('resize', () => {
+        if (document.body.clientWidth > 992) setIsMenuOpen(true);
+    });
+
+    return (
+        <div className="main-layout">
+            {isMenuOpen && <Menu setIsMenuOpen={setIsMenuOpen} />}
+
+            <div className="main-layout-content">
+                <div
+                    className="mobile-header"
+                    onClick={() => setIsMenuOpen(true)}
+                >
+                    <img className="mobile-header-user-photo" src={userPhoto} alt="user photo sample" />
+
+                    <Logo className="mobile-header-logo" />
+                </div>
+
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default MainLayout;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
+import MainLayout from "./layouts/main";
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Landing />} />
+                <Route path="/home" element={<MainLayout children={<h1>MindLab home page</h1>} />} />
+                <Route path="/lessons" element={<MainLayout children={<h1>MindLab lessons page</h1>} />} />
+                <Route path="/performance" element={<MainLayout children={<h1>MindLab performance page</h1>} />} />
+                <Route path="/materials" element={<MainLayout children={<h1>MindLab materials page</h1>} />} />
+                <Route path="/forums" element={<MainLayout children={<h1>MindLab forums page</h1>} />} />
+                <Route path="/achievements" element={<MainLayout children={<h1>MindLab achievements page</h1>} />} />
+                <Route path="/personalization" element={<MainLayout children={<h1>MindLab personalization page</h1>} />} />
             </Routes>
         </Router>
     );
