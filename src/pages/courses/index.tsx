f0eba14cf1d4621c6cc85d65d093a1964dff1cb6
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2023-06-26 20:03:55 -0300

    Courses page

---
+++ src/pages/courses/index.tsx
@@ -0,0 +1,73 @@
+import { useNavigate } from "react-router-dom";
+import planning from "../../assets/common/course-cards/planning.png";
+import design from "../../assets/common/course-cards/design.png";
+import programming from "../../assets/common/course-cards/programming.png";
+import frontend from "../../assets/common/course-cards/frontend.png";
+import backend from "../../assets/common/course-cards/backend.png";
+import pixelArt from "../../assets/common/course-cards/pixel-art.png";
+import scratch from "../../assets/common/course-cards/scratch.png";
+import cientificMetodology from "../../assets/common/course-cards/cientific-metodology.png";
+import "./styles.css";
+
+interface ICourseItem {
+    imgSrc: string;
+    path: string;
+}
+
+function Courses() {
+    const navigate = useNavigate();
+
+    const courseItems: ICourseItem[] = [
+        {   
+            imgSrc: planning,
+            path: "/planning"
+        },
+        {   
+            imgSrc: design,
+            path: "/design"
+        },
+        {   
+            imgSrc: programming,
+            path: "/programming"
+        },
+        {   
+            imgSrc: frontend,
+            path: "/frontend"
+        },
+        {   
+            imgSrc: backend,
+            path: "/backend"
+        },
+        {   
+            imgSrc: pixelArt,
+            path: "/pixel-art"
+        },
+        {   
+            imgSrc: scratch,
+            path: "/scratch"
+        },
+        {   
+            imgSrc: cientificMetodology,
+            path: "/cientific-metodology"
+        },
+    ]
+
+    return (
+        <div className="courses">
+            <div className="courses-blur-wrapper">
+                {courseItems.map(({ imgSrc, path }) => (
+                    <div 
+                        className="course-item"
+                        onClick={() => navigate(`/courses${path}`)}
+                    >
+                        <div className="course-item-blur" />
+
+                        <img src={imgSrc} alt={path} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Courses;
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
 import MainLayout from "./layouts/main";
 import Login from "./pages/login";
+import Courses from "./pages/courses";
 
 function AppRoutes() {
     return (
@@ -10,7 +11,7 @@ function AppRoutes() {
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<MainLayout children={<h1>MindLab home page</h1>} />} />
-                <Route path="/lessons" element={<MainLayout children={<h1>MindLab lessons page</h1>} />} />
+                <Route path="/courses" element={<MainLayout children={<Courses />} />} />
                 <Route path="/performance" element={<MainLayout children={<h1>MindLab performance page</h1>} />} />
                 <Route path="/materials" element={<MainLayout children={<h1>MindLab materials page</h1>} />} />
                 <Route path="/forums" element={<MainLayout children={<h1>MindLab forums page</h1>} />} />
