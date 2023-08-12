d94ec8a7a62482f0cbdb2e28fa37bfec235e3ac0
HenriqueSenaDev <acopsenadeveloper@gmail.como>
2023-08-12 11:25:45 -0300

    Personalization page desktop structure and styles

--- src/pages/personalization/index.tsx
+++ src/pages/personalization/index.tsx
@@ -3,8 +3,15 @@ import { ReactComponent as SecondaryTopShape } from "../../assets/personalizatio
 import { ReactComponent as PrimaryBottomShape } from "../../assets/personalization/primary-bottom-shape.svg";
 import { ReactComponent as SecondaryBottomShape } from "../../assets/personalization/secondary-bottom-shape.svg";
 import { ReactComponent as CheckIcon } from "../../assets/personalization/check.svg";
-import "./styles.css";
+import { ReactComponent as PlayButtonIcon } from "../../assets/lesson/play.svg";
+import { ReactComponent as SearchIcon } from "../../assets/personalization/demo/search.svg";
+import { ReactComponent as DemoCategoriesIcon } from "../../assets/personalization/demo/categories.svg";
+import { ReactComponent as DemoSampleWolf } from "../../assets/personalization/demo/sample-wolf.svg";
+import { ReactComponent as BottomWolf } from "../../assets/personalization/bottom-wolf.svg";
+import themeDemoMonitor from "../../assets/personalization/demo/monitor.png";
+import nextLessonArrow from "../../assets/personalization/demo/arrow.png";
 import ColorSelector from "./components/color-selector";
+import "./styles.css";
 
 function Personalization() {
   return (
@@ -25,7 +32,7 @@ function Personalization() {
             <h1 className="color-title">Cor Principal</h1>
 
             <div className="color-definition primary-definition">
-              <div className="color-sample"/>
+              <div className="color-sample" />
 
               <div className="color-hex">
                 <span>Hexadecimal</span>
@@ -39,53 +46,19 @@ function Personalization() {
             <h1 className="color-title">Cor Secundária</h1>
 
             <div className="color-definition secondary-definition">
-                <div className="color-sample"/>
+              <div className="color-sample" />
 
-                <div className="color-hex">
-                  <span>Hexadecimal</span>
+              <div className="color-hex">
+                <span>Hexadecimal</span>
 
-                  <h1>#F3701F</h1>
-                </div>
+                <h1>#F3701F</h1>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="palete-explanation">
           <span>Descubra uma paleta de cores única: <strong>escolha a cor principal</strong> que define a essência da plataforma, e <strong>a cor secundária será a sua contrapartida complementar</strong>. Transforme sua experiência em algo verdadeiramente excepcional!</span>
-        </div>
-
-        <div className="colors-descriptor-container">
-          <div className="color-descriptor">
-            <h1 className="color-title">
-              Light Mode
-            </h1>
-
-            <div className="color-definition light-definition">
-              <div className="color-sample"/>
-
-              <div className="color-hex">
-                <span>Hexadecimal</span>
-
-                <h1>#0093CE</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="color-descriptor">
-            <h1 className="color-title">
-              Dark Mode
-            </h1>
-
-            <div className="color-definition dark-definition">
-                <div className="color-sample"/>
-
-                <div className="color-hex">
-                  <span>Hexadecimal</span>
-
-                  <h1>#F3701F</h1>
-                </div>
-            </div>
-          </div>
         </div>
 
         <button className="theme-visualizer">
@@ -98,6 +71,57 @@ function Personalization() {
           APLICAR
         </button>
       </div>
+
+      <div className="theme-demo-area">
+        <img
+          className="theme-demo-monitor"
+          src={themeDemoMonitor}
+          alt="theme demo monitor"
+        />
+
+        <div className="demo-sample-container">
+          <div className="main-demo-area">
+            <div className="demo-lesson-thumbnail">
+              <button className="demo-play-button">
+                <PlayButtonIcon className="demo-play-icon" />
+              </button>
+            </div>
+
+            <h1>Categorias</h1>
+
+            <div className="demo-lesson-cards">
+              <div className="demo-lesson-card" />
+              <div className="demo-lesson-card" />
+              <div className="demo-lesson-card" />
+              <div className="demo-lesson-card" />
+
+              <button className="demo-next-button">
+                <img src={nextLessonArrow} alt="next lesson arrow" />
+              </button>
+            </div>
+          </div>
+
+          <div className="aside-demo-area">
+            <div className="demo-search-bar">
+              <SearchIcon className="demo-search-icon" />
+            </div>
+
+            <h1>Categorias</h1>
+
+            <DemoCategoriesIcon className="demo-categories" />
+
+            <DemoSampleWolf className="demo-sample-wolf" />
+          </div>
+        </div>
+
+        <button className="desktop-apply-theme">
+          <CheckIcon className="check-icon" />
+
+          APLICAR
+        </button>
+      </div>
+
+      <BottomWolf className="bottom-wolf" />
     </div>
   );
 }
